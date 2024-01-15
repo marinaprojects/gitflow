1494b081c43a1ef3d7067f7aae91da7c82b9882f
Marina Safina <safina531@gmail.com>
2024-01-15 06:37:17 -0600

    added def docstring_transformation

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def capitalize_string(input_string):
     return input_string.upper()
+
+def docstring_transformation(input_string):
+    """
+    Делает заглавными первые буквы каждого слова в строке
+    """
+    return input_string.title()
