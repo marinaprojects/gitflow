7668045f417f856f276017c07f6a729160f3e4ee
marinaprojects <155964514+marinaprojects@users.noreply.github.com>
2024-01-16 04:09:43 -0600

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -4,8 +4,4 @@ def capitalize_string(input_string):
     """
     return input_string.upper()
 
-def docstring_transformation(input_string):
-    """
-    Делает заглавными первые буквы каждого слова в строке
-    """
-    return input_string.title()
+
